c331283830336f3f6fc67c6bedacdd4db3495319
79150 <12345ok6@mail.ru>
2020-06-02 13:55:52 +0300

    02.06.2020

--- app/src/main/java/ru/ostapenkoyanko/myflashlight/MainActivity.java
+++ app/src/main/java/ru/ostapenkoyanko/myflashlight/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -15,7 +14,6 @@ import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
-
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
@@ -31,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewPushMe;
     private TextView textViewEng;
     private TextView textViewRus;
-    private SeekBar BackLightControl;
-    private TextView BackLightSetting;
+    private SeekBar backLightControl;
+    private TextView backLightSetting;
     private float saveBackLightValue;
     float defaultBrightness;
 
@@ -54,11 +52,13 @@ public class MainActivity extends AppCompatActivity {
         saveBackLightValue = defaultBrightness;
 
 
-        BackLightControl = findViewById(R.id.backlightcontrol);
-        BackLightSetting = findViewById(R.id.backlightsetting);
+        backLightControl = findViewById(R.id.backlightcontrolid);
+        backLightSetting = findViewById(R.id.backlightsettingid);
+
+        backLightControl.setVisibility(View.INVISIBLE);
 
 
-        BackLightControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+        backLightControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
             @Override
             public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 float BackLightValue = (float)arg1/100;
                 System.out.println("arg1 = "+ arg1);
                 System.out.println("BackLightValue = "+ BackLightValue);
-                BackLightSetting.setText(String.valueOf(BackLightValue));
+                backLightSetting.setText(String.valueOf(BackLightValue));
                 WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
                 layoutParams.screenBrightness = BackLightValue;
                 getWindow().setAttributes(layoutParams);
@@ -93,11 +93,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    textViewVeryScary.setText("ОЧЕНЬ СТРАШНО");
-                    textViewNotScary.setText("ТЕПЕРЬ НЕ СТРАШНО");
-                    textViewPushMe.setText("Нажми на меня");
-                    textViewEng.setText("Анг");
-                    textViewRus.setText("Рус");
+                    textViewVeryScary.setText(R.string.strashno);
+                    textViewNotScary.setText(R.string.ne_strashno);
+                    textViewPushMe.setText(R.string.najmi);
+                    textViewEng.setText(R.string.ang);
+                    textViewRus.setText(R.string.russkii);
                 } else {
                     textViewVeryScary.setText(R.string.VeryScary);
                     textViewNotScary.setText(R.string.not_scary);
@@ -113,36 +113,30 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
                 if(!flashClass.isFlash_on()) {
-
-                    WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
                     layoutParams.screenBrightness = saveBackLightValue;
                     getWindow().setAttributes(layoutParams);
-
                     flashClass.flashOn();
                     setTunerPopoverSize2();
                     constFon.setBackgroundResource(R.color.colorPrimary);
                     textViewPushMe.setTextColor(getResources().getColor(R.color.colorPrimaryDark, getTheme()));
                     textViewEng.setTextColor(getResources().getColor(R.color.colorPrimaryDark, getTheme()));
                     textViewRus.setTextColor(getResources().getColor(R.color.colorPrimaryDark, getTheme()));
-
-
-
+                    backLightControl.setVisibility(View.VISIBLE);
+                    textViewVeryScary.setVisibility(View.INVISIBLE);
                 }
                 else {
-
-                    WindowManager.LayoutParams defaultParams = getWindow().getAttributes();
-                    defaultParams.screenBrightness = defaultBrightness;
-                    getWindow().setAttributes(defaultParams);
-
+                    layoutParams.screenBrightness = defaultBrightness;
+                    getWindow().setAttributes(layoutParams);
                     flashClass.flashOff();
                     setTunerPopoverSize();
                     constFon.setBackgroundResource(R.color.colorPrimaryDark);
                     textViewPushMe.setTextColor(getResources().getColor(R.color.colorPrimary, getTheme()));
                     textViewEng.setTextColor(getResources().getColor(R.color.colorPrimary, getTheme()));
                     textViewRus.setTextColor(getResources().getColor(R.color.colorPrimary, getTheme()));
-                    BackLightControl.getThumb().setColorFilter(getResources().getColor(R.color.colorPrimaryDark, getTheme()), PorterDuff.Mode.MULTIPLY);
-                    BackLightControl.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimaryDark, getTheme()), PorterDuff.Mode.MULTIPLY);
+                    backLightControl.setVisibility(View.INVISIBLE);
+                    textViewVeryScary.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -158,7 +152,6 @@ public class MainActivity extends AppCompatActivity {
             textViewPushMe.setTextSize(12);
             textViewEng.setTextSize(12);
             textViewRus.setTextSize(12);
-            // set button size here if high density
         }  else if (screenDensity == DisplayMetrics.DENSITY_MEDIUM) {
             bFlash.setImageResource(R.drawable.cust_button_default);
             textViewVeryScary.setTextSize(26);
@@ -166,7 +159,6 @@ public class MainActivity extends AppCompatActivity {
             textViewPushMe.setTextSize(20);
             textViewEng.setTextSize(20);
             textViewRus.setTextSize(20);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_HIGH) {
             bFlash.setImageResource(R.drawable.cust_button_default);
             textViewVeryScary.setTextSize(26);
@@ -174,7 +166,6 @@ public class MainActivity extends AppCompatActivity {
             textViewPushMe.setTextSize(20);
             textViewEng.setTextSize(20);
             textViewRus.setTextSize(20);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_XHIGH) {
             bFlash.setImageResource(R.drawable.cust_button_large);
             textViewVeryScary.setTextSize(36);
@@ -182,7 +173,6 @@ public class MainActivity extends AppCompatActivity {
             textViewPushMe.setTextSize(26);
             textViewEng.setTextSize(26);
             textViewRus.setTextSize(26);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_XXHIGH) {
             bFlash.setImageResource(R.drawable.cust_button_large);
             textViewVeryScary.setTextSize(36);
@@ -190,7 +180,6 @@ public class MainActivity extends AppCompatActivity {
             textViewPushMe.setTextSize(26);
             textViewEng.setTextSize(26);
             textViewRus.setTextSize(26);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_XXXHIGH) {
             bFlash.setImageResource(R.drawable.cust_button_large);
             textViewVeryScary.setTextSize(36);
@@ -198,13 +187,11 @@ public class MainActivity extends AppCompatActivity {
             textViewPushMe.setTextSize(26);
             textViewEng.setTextSize(26);
             textViewRus.setTextSize(26);
-            // set button size here if xhigh density
         } else{
             bFlash.setImageResource(R.drawable.cust_button_default);
             textViewVeryScary.setTextSize(26);
             textViewNotScary.setTextSize(26);
             textViewPushMe.setTextSize(20);
-            // set default button size
         }
     }
 
@@ -213,25 +200,18 @@ public class MainActivity extends AppCompatActivity {
 
         if (screenDensity == DisplayMetrics.DENSITY_LOW) {
             bFlash.setImageResource(R.drawable.cust_button2_small);
-            // set button size here if high density
         }  else if (screenDensity == DisplayMetrics.DENSITY_MEDIUM) {
             bFlash.setImageResource(R.drawable.cust_button2_default);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_HIGH) {
             bFlash.setImageResource(R.drawable.cust_button2_default);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_XHIGH) {
             bFlash.setImageResource(R.drawable.cust_button2_large);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_XXHIGH) {
             bFlash.setImageResource(R.drawable.cust_button2_large);
-            // set button size here if xhigh density
         } else if (screenDensity == DisplayMetrics.DENSITY_XXXHIGH) {
             bFlash.setImageResource(R.drawable.cust_button2_large);
-            // set button size here if xhigh density
         } else{
             bFlash.setImageResource(R.drawable.cust_button2_default);
-            // set default button size
         }
     }
 
